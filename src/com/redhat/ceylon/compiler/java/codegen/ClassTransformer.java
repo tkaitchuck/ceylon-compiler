599b1e8e3ef179e3b52e91f6438938b035a7fc45
Tom Bentley <tom.bentley@cantab.net>
2013-07-29 11:30:11 +0100

    ClassTransformer: ws

--- src/com/redhat/ceylon/compiler/java/codegen/ClassTransformer.java
+++ src/com/redhat/ceylon/compiler/java/codegen/ClassTransformer.java
@@ -2145,7 +2145,7 @@ public class ClassTransformer extends AbstractTransformer {
         boolean prevNoExpressionlessReturn = statementGen().noExpressionlessReturn;
         try {
             statementGen().noExpressionlessReturn = Decl.isMpl(model) || Strategy.useBoxedVoid(model);
-            body = statementGen().transform(block).getStatements();    
+            body = statementGen().transform(block).getStatements();
         } finally {
             statementGen().noExpressionlessReturn = prevNoExpressionlessReturn;
         }
